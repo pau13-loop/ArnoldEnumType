e7dbca2d6f798ee1c402a6693b88589db5f07c10
pau <pllinas@cifpfbmoll.eu>
2021-03-03 23:57:41 +0100

    feat: :sparkles: Added user stories

--- arnoldEnumType/src/main/java/edu/pingpong/arnoldEnumType/main/ArnoldApp.java
+++ arnoldEnumType/src/main/java/edu/pingpong/arnoldEnumType/main/ArnoldApp.java
@@ -1,13 +1,24 @@
 package edu.pingpong.arnoldEnumType.main;
 
-/**
- * Hello world!
- *
- */
-public class ArnoldApp 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import edu.pingpong.arnoldEnumType.domain.*;
+
+public class ArnoldApp {
+    public static void main( String[] args ) {
+
+        double peso = 1.0; // kg
+		
+        for(Planeta planeta : Planeta.values()){
+            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
+        
+        System.out.println("\nYour weight only on the terrestrial planets: ");
+        for(Planeta planeta: Planeta.getPlanetasTerrestres()){
+            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
+        
+        System.out.println("\nYour weight only on the gas giant planets: ");
+        for(Planeta planeta: Planeta.getGigantesGaseosos()){
+            System.out.printf("Your weight on %s is %f N%n", planeta.name(), planeta.pesoSuperficie(peso));
+        }
     }
 }
