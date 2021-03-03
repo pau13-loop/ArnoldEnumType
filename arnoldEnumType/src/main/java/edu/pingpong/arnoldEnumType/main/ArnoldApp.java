a54fd24ab47c074e443fde461c9597efdc7d6a71
pau <pllinas@cifpfbmoll.eu>
2021-03-03 23:56:39 +0100

    feat: :sparkles: Deployment project

---
+++ arnoldEnumType/src/main/java/edu/pingpong/arnoldEnumType/main/ArnoldApp.java
@@ -0,0 +1,13 @@
+package edu.pingpong.arnoldEnumType.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class ArnoldApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
